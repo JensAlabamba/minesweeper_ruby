c5ecf8a6241754fe7d19c8bca8cc62e43f863742
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-11-27 16:06:45 +0100

    empty grid setup

--- board.rb
+++ board.rb
@@ -1,3 +1,11 @@
 class Board
 
+    def self.empty_grid
+        Array.new(9) do 
+            Arry.new(9) { Chunk.new(0) }
+    end
+
+    def initialize(grid = self.empty_grid)
+        @grid = grid
+    end
 end
