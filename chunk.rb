99079b885c5ebcab7294a9e230622af90a2cbe72
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-11-29 14:30:24 +0100

    initialize and AROUND set

--- chunk.rb
+++ chunk.rb
@@ -1,4 +1,19 @@
 class Chunk
     
-    
+    AROUND = [
+        [-1, -1],
+        [-1,  0],
+        [-1,  1],
+        [ 0, -1],
+        [ 0,  1],
+        [ 1, -1],
+        [ 1,  0],
+        [ 1,  1]
+      ].freeze
+
+      def initialize(board, pos)
+        @board = board
+        @pos = pos
+        @bombed , @flagged, @explored = false, false, false
+      end
 end
